f6091b542a5148a3350c73df56f86fd129c157aa
kwrona05 <kwrona05@gmail.com>
2025-04-19 19:12:40 +0200

    Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
+import Home from './components/Home.tsx'
+import Articles from "./components/Articles.tsx";
+import AdminPage from "./cms/page.tsx";
 
 const App = () => {
     return(
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home/>} />
+                <Route path="/posts" element={<Articles />} />
+                <Route path="/page" element={<AdminPage />} />
             </Routes>
         </Router>
     )
 }
+
+export default App;
