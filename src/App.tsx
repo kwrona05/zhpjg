fe01d185d4261dbb038fea7a5db89d700954dad1
kwrona05 <kwrona05@gmail.com>
2025-04-21 19:09:03 +0200

    Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/Home.tsx'
 import Articles from "./components/Articles.tsx";
 import AdminPage from "./cms/page.tsx";
+import HistoricComission from "./components/HistoricComission.tsx";
+import Panteon from "./components/Panteon";
+import Publications from "./components/Publications.tsx";
+import Banners from "./components/Banners.tsx";
+import Museum from "./components/Museum.tsx";
+import Flag from "./components/Flag.tsx";
+import Detachment from "./components/Detachment.tsx";
+import Contact from "./components/Contact.tsx";
+import FEN from "./components/FEN.tsx";
 
 const App = () => {
     return(
@@ -10,6 +19,15 @@ const App = () => {
                 <Route path="/" element={<Home/>} />
                 <Route path="/posts" element={<Articles />} />
                 <Route path="/page" element={<AdminPage />} />
+                <Route path='/komisja-historyczna' element={<HistoricComission />} />
+                <Route path='/panteon' element={<Panteon />} />
+                <Route path='/publikacje' element={<Publications />} />
+                <Route path='/sztandary' element={<Banners />} />
+                <Route path='/muzeum' element={<Museum />} />
+                <Route path='/choragiew' element={<Flag />} />
+                <Route path='/hufiec' element={<Detachment />} />
+                <Route path='/kontakt' element={<Contact />} />
+                <Route path='/fen' element={<FEN />} />
             </Routes>
         </Router>
     )
