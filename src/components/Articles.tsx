9ad9be46908657567fe511c80cdacd52ac81fd3d
kwrona05 <kwrona05@gmail.com>
2025-04-21 08:45:02 +0200

    fix Article component with endpoints

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,76 +1,34 @@
-// import {useEffect, useState} from "react";
-// import axios from 'axios'
-//
-// const Articles = () => {
-//     const [posts, setPosts] = useState([])
-//
-//     const fetchPosts = async () => {
-//         const res = await axios.get('http://localhost:4000/api/posts')
-//         setPosts(res.data)
-//     }
-//
-//     useEffect(() => {
-//         fetchPosts()
-//     }, [])
-//
-//     return(
-//         <main>
-//             {posts.length === 0 ? (
-//                 <p>Brak wpisów</p>
-//             ) : (
-//                 posts.map((post) => (
-//                     <article key={post.id}>
-//                         <h2>{post.title}</h2>
-//                         <p>{post.content}</p>
-//                         <img src={`http://localhost:4000/uploads/${post.image}`} alt="Obraz"/>
-//                     </article>
-//                 ))
-//             )}
-//         </main>
-//     )
-// }
-//
-// export default Articles
-
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from "react";
 import axios from 'axios'
 
-const Posts = () => {
+const Articles = () => {
     const [posts, setPosts] = useState([])
 
+    const fetchPosts = async () => {
+        const res = await axios.get('http://localhost:4000/api/posts')
+        setPosts(res.data)
+    }
+
     useEffect(() => {
-        axios.get('http://localhost:1337/api/posts?populate=*')
-            .then(res => {
-                console.log("Dane z API:", res.data.data)
-                setPosts(res.data.data)
-            })
-            .catch(err => console.error(err))
+        fetchPosts()
     }, [])
 
-    return (
-        <div>
-            <h1>Posty</h1>
-            {posts.map(post => {
-                const attributes = post.attributes
-                if (!attributes) return null
-
-                return (
-                    <div key={post.id}>
-                        <h2>{attributes.title}</h2>
-                        <p>{attributes.content}</p>
-                        {attributes.image?.data && (
-                            <img
-                                src={`http://localhost:1337${attributes.image.data.attributes.url}`}
-                                alt={attributes.title}
-                                width="300"
-                            />
-                        )}
-                        <small>Kategoria: {attributes.category?.data?.attributes?.name}</small>
-                    </div>
-                )
-            })}
-        </div>
+    return(
+        <main>
+            {posts.length === 0 ? (
+                <p>Brak wpisów</p>
+            ) : (
+                posts.map((post) => (
+                    <article key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.content}</p>
+                        <img src={`http://localhost:4000/uploads/${post.image}`} alt="Obraz"/>
+                        <button onClick={() => console.log(post.image)}>Click</button>
+                    </article>
+                ))
+            )}
+        </main>
     )
 }
 
-export default Posts
+export default Articles
